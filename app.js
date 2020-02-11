6e3f11403ba035edebc65613c1f3b2cf609da8d8
John Paguirigan <johnjaroldxvi@gmail.com>
2020-02-10 18:05:40 -0700

    added animation and background colors

--- app.js
+++ app.js
@@ -1,65 +1,71 @@
 const colors = {
-  fire: '#FDDFDF',
-  grass: '#DEFDE0',
-  electric: '#FCF7DE',
-  water: '#DEF3FD',
-  ground: '#f4e7da',
-  rock: '#d5d5d4',
-  fairy: '#fceaff',
-  poison: '#98d7a5',
-  bug: '#f8d5a3',
-  dragon: '#97b3e6',
-  psychic: '#eaeda1',
-  flying: '#F5F5F5',
-  fighting: '#E6E0D4',
-  normal: '#F5F5F5'
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5"
 };
 
-const pokedex_container = document.getElementById('pokedex-content')
-const pokemon_number = 150
-const main_types = Object.keys(colors)
+const pokedex_container = document.getElementById("pokedex-content");
+let pokemon_number = 150;
+const main_types = Object.keys(colors);
+const body = document.getElementsByTagName("body");
 
-
+// FOR THE URL'S ITERATION
 
 const fetchPokemon = async () => {
   for (let i = 1; i <= pokemon_number; i++) {
-    await catchPokemon(i)
+    await catchPokemon(i);
   }
-}
+};
 
+// TO FETCH API
 
 const catchPokemon = async id => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-  const res = await fetch(url)
-  const pokemon = await res.json()
-  generatePokemon(pokemon)
-}
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  generatePokemon(pokemon);
+};
 
-const generatePokemon = (pokemon) => {
-  const pokemonElement = document.createElement('div')
-  pokemonElement.classList.add('pokemon')
-  const pokemon_types = pokemon.types.map(element => element.type.name)
-  const type = main_types.find(type => pokemon_types.indexOf(type) > -1)
+// TEMPLATE OF OUR POKEMON CARDS
 
+const generatePokemon = pokemon => {
+  const pokemonElement = document.createElement("div");
+  pokemonElement.classList.add("pokemon");
+  const pokemon_types = pokemon.types.map(element => element.type.name);
+  const type = main_types.find(type => pokemon_types.indexOf(type) > -1);
 
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const color = colors[type];
 
+  pokemonElement.style.backgroundColor = color;
   const html = `
 
     <div class="pokemonImage">
-    <img src = "https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+    <img src = "https://pokeres.bastionbot.org/images/pokemon/${
+      pokemon.id
+    }.png" />
     </div>
 
     <div class="info">
-      <span class="pokemon-id>$ {pokemon.id}</span>
+      <span class="pokemon-id">${pokemon.id.toString().padStart(3, "0")}</span> 
       <h3 class="pokemon-name">${name}</h3>
-      <span class="pokemon-type">Type: ${pokemon_types.join(', ')}</span>
+      <p class="pokemon-type">Type: ${pokemon_types.join(", ")}</p>
     </div>
-  `
+  `;
 
+  pokemonElement.innerHTML = html;
+  pokedex_container.appendChild(pokemonElement);
+};
 
-
-  pokemonElement.innerHTML = html
-  pokedex_container.appendChild(pokemonElement)
-}
-fetchPokemon()
+fetchPokemon();
