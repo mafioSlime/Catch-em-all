75986e48ea9b98d060fb9b487358f90e071995b1
John Paguirigan <johnjaroldxvi@gmail.com>
2020-02-11 23:10:53 -0700

    finalized project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const colors = {
-  fire: "#FDDFDF",
+  fire: "#f20048",
   grass: "#DEFDE0",
   electric: "#FCF7DE",
   water: "#DEF3FD",
@@ -16,15 +16,8 @@ const colors = {
 };
 
 const pokedex_container = document.getElementById("pokedex-content");
-const pokemon_number = 150;
-const random = Math.floor(Math.random() * 20 + 1);
+const pokemon_number = 251;
 const main_types = Object.keys(colors);
-const body = document.getElementsByTagName("body");
-const btn = document.querySelector(".button");
-
-// btn.addEventListener("click", () => {
-
-// });
 
 // FOR THE URL'S ITERATION
 
@@ -57,13 +50,13 @@ const generatePokemon = pokemon => {
   pokemonElement.style.backgroundColor = color;
   const html = `
 
-    <div class="pokemonImage">
+    <div class="pokemonImage" onclick="selectPokemon(${pokemon.id})">
     <img src = "https://pokeres.bastionbot.org/images/pokemon/${
       pokemon.id
     }.png" />
     </div>
 
-    <div class="info">
+    <div class="info" onclick="selectPokemon(${pokemon.id})">
       <span class="pokemon-id">${pokemon.id.toString().padStart(3, "0")}</span> 
       <h3 class="pokemon-name">${name}</h3>
       <p class="pokemon-type">Type: ${pokemon_types.join(", ")}</p>
@@ -74,6 +67,47 @@ const generatePokemon = pokemon => {
   pokedex_container.appendChild(pokemonElement);
 };
 
+const selectPokemon = async id => {
+  let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  displayIndividual(pokemon);
+};
+
+const displayIndividual = pokemon => {
+  const pokemon_types = pokemon.types.map(element => element.type.name);
+  const type = main_types.find(type => pokemon_types.indexOf(type) > -1);
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const color = colors[type];
+
+  const htmlString = `
+  <div class="popup" style="background-color:${color}">
+    <button class="close" onclick="closePopup()"><i class="fas fa-times fa-2x"></i></button>
+    <div class="pokemonImage">
+    <img src = "https://pokeres.bastionbot.org/images/pokemon/${
+      pokemon.id
+    }.png" />
+    </div>
+
+    <div class="info">
+      <span class="pokemon-id">${pokemon.id.toString().padStart(3, "0")}</span> 
+      <h3 class="pokemon-name">${name}</h3>
+      <p class><span>Height: ${pokemon.height}</span> |
+       <span>Weight: ${pokemon.weight}</span></p>
+      <p><span>Base Experience: ${pokemon.base_experience}</span></p>
+      <p class="pokemon-type">Type: ${pokemon_types.join(", ")}</p>
+    </div>
+  
+ </div>
+`;
+  console.log(pokemon.version);
+  pokedex_container.innerHTML = htmlString + pokedex_container.innerHTML;
+};
+
+const closePopup = () => {
+  const popup = document.querySelector(".popup");
+  popup.parentElement.removeChild(popup);
+};
 //LOADING
 
 const rotate = document.querySelector(".rotate");
